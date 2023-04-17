2157615736f6874f52288bfe39ab2140456c63e9
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-04-17 19:25:04 -0300

    Criação Classe Planeta

---
+++ app/src/main/java/com/jhonssantiago/sistema_solar_com_spinner/Planeta.java
@@ -0,0 +1,39 @@
+package com.jhonssantiago.sistema_solar_com_spinner;
+
+public class Planeta {
+    private int imagem;
+    private String nome;
+    private String descricao;
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Planeta(int imagem, String nome, String descricao) {
+        this.imagem = imagem;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+
+}
